9bf97b7f8ee9039f73cc5e83d4a77c53865bbde1
Shehbaz Alam <shehbazbalghari@gmail.com>
2024-07-23 17:12:00 +0500

    updated site ui

--- bird_voice_classifier/urls.py
+++ bird_voice_classifier/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import predict, signup, index
+from .views import index
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('predict', predict),
-    path('signup', signup),
     path('', index),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- bird_voice_classifier/views.py
+++ bird_voice_classifier/views.py
@@ -1,23 +1,4 @@
 from django.shortcuts import render
 
-class site():
-    def __init__(self) -> None:
-        pass
-
-    #function to presict the sound
-    def predict(sound):
-        pass
-
-    #function to review prediction
-    def review_prediction(user, review):
-        pass
-
-
-def predict(request):
-    return render(request, 'site/upload.html')
-
-def signup(request):
-    return render(request, 'site/signup.html')
-
 def index(request):
     return render(request, 'site/index.html')
