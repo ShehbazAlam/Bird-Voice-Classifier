72cded7144d4069f447921c0fa27d46bb9ab9432
Shehbaz Alam <shehbazbalghari@gmail.com>
2024-07-23 17:37:40 +0500

    updated site ui

--- bird_voice_classifier/urls.py
+++ bird_voice_classifier/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, result
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="home"),
+    path('result', result, name="result"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- bird_voice_classifier/views.py
+++ bird_voice_classifier/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'site/index.html')
+
+def result(request):
+    return render(request, 'site/result.html')
