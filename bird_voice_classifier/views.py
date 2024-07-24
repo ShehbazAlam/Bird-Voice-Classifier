727a2b356adcc50107335af6b674f8bf4e3354e7
Shehbaz Alam <shehbazbalghari@gmail.com>
2024-07-24 19:36:40 +0500

    implementing logics

--- bird_voice_classifier/views.py
+++ bird_voice_classifier/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from model import predict
 from .forms import FileUploadForm
+from django.core.files.storage import FileSystemStorage
+
+import os
+
+import random
+import string
+
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
 
 def index(request):
     form = FileUploadForm
     return render(request, 'site/index.html', {"form": form})
 
 def result(request):
-    if request.method == "POST":
-        form = FileUploadForm(request.POST, request.FILES)
+    if request.method == "POST" and request.FILES['bird-sound']:
         file = request.FILES['bird-sound']
         
-        prediction = predict.prediction(file)
+        fs = FileSystemStorage()
+        dir = './static/uploads'
+        file_name = 'upload-'+generate_random_string(5)+'.mp3'
+        file_path = f'{dir}/{file_name}'
+        filename = fs.save(file_path, file)
+        uploaded_file_url = fs.url(filename)
+        prediction = predict.prediction(uploaded_file_url)
 
         print(prediction)
         return render(request, 'site/result.html', {'prediction': prediction})
