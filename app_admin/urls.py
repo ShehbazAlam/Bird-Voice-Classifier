894bd387c94fe180665cdba9faf5d0587f343a46
Shehbaz Alam <shehbazbalghari@gmail.com>
2024-07-31 19:23:11 +0500

    initialized app-admin files

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+from app_admin.views import dashboard, user_list, user_detail, predications_list, prediction_detail
 
+urlpatterns = [
+    path('dashboard', dashboard, name="dashboard"),
+    path('user-list', user_list, name='user_list'),
+    path('user-details', user_detail, name='user_detail'),
+    path('predictions-list', predications_list, name="prediction_list"),
+    path('prediction-detail', prediction_detail, name='prediction_detail')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app_admin/views.py
+++ app_admin/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard(request):
+    pass
+
+def user_list(request):
+    pass
+
+def user_detail(request):
+    pass
+
+def predications_list(request):
+    pass
+
+def prediction_detail(request):
+    pass
